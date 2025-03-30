18730e1179b3d92395d6ca02fc633fce213df05d
Quabena_the_dev <ebenezer.gyamfi@regent.edu.gh>
2025-03-30 09:12:48 +0000

    display cart item count and cart items for user

--- Cart/views.py
+++ Cart/views.py
@@ -120,12 +120,16 @@ def delete_cart_item(request, product_id):
     return redirect('cart')
 
 def cart(request, total=0, quantity=0, cart_items=None):
-    tax = 0
-    grand_total = 0
+   
     try:
-        cart = Cart.objects.get(cart_id=cart_id(request))
-        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
-        
+        tax = 0
+        grand_total = 0
+        if request.user.is_authenticated: 
+            cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+        else:
+            cart = Cart.objects.get(cart_id=cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+            
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
