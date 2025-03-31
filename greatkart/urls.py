bc7d11daae59b50988812dc7f3d3184fc59bbd67
Quabena_the_dev <ebenezer.gyamfi@regent.edu.gh>
2025-03-31 11:58:43 +0000

    wip

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('store/', include('store.urls')),
     path('cart/', include('Cart.urls')),
     path('accounts/', include('accounts.urls')),
+    path('orders/', include('orders.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
